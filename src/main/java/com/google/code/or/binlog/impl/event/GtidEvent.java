f6296937f83caa8e883c1166dcd8e67a11126e7b
Jingqi Xu <xujq@nextop.cn>
2016-01-04 23:40:13 +0800

    gtid event

--- src/main/java/com/google/code/or/binlog/impl/event/GtidEvent.java
+++ src/main/java/com/google/code/or/binlog/impl/event/GtidEvent.java
@@ -1,20 +1,72 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.code.or.binlog.impl.event;
 
-public class GtidEvent extends AbstractBinlogEventV4
-{
-  private final byte[] sourceId;
-  private final long transactionId;
+import java.util.Arrays;
 
-  public GtidEvent(byte[] sourceId, long transactionId) {
-    this.sourceId = sourceId;
-    this.transactionId = transactionId;
-  }
+import com.google.code.or.binlog.BinlogEventV4Header;
+import com.google.code.or.common.util.ToStringBuilder;
 
-  public byte[] getSourceId() {
-    return sourceId;
-  }
+/**
+ * 
+ * @author brandtg
+ */
+public class GtidEvent extends AbstractBinlogEventV4 {
+	//
+	private byte[] sourceId;
+	private long transactionId;
 
-  public long getTransactionId() {
-    return transactionId;
-  }
+	/**
+	 * 
+	 */
+	public GtidEvent() {
+	}
+	
+	public GtidEvent(BinlogEventV4Header header) {
+		this.header = header;
+	}
+
+	/**
+	 * 
+	 */
+	@Override
+	public String toString() {
+		return new ToStringBuilder(this)
+		.append("header", header)
+		.append("transactionId", transactionId)
+		.append("sourceId", Arrays.toString(sourceId)).toString();
+	}
+	
+	/**
+	 * 
+	 */
+	public byte[] getSourceId() {
+		return sourceId;
+	}
+
+	public void setSourceId(byte[] sourceId) {
+		this.sourceId = sourceId;
+	}
+
+	public long getTransactionId() {
+		return transactionId;
+	}
+
+	public void setTransactionId(long transactionId) {
+		this.transactionId = transactionId;
+	}
 }
--- src/main/java/com/google/code/or/binlog/impl/parser/GtidEventParser.java
+++ src/main/java/com/google/code/or/binlog/impl/parser/GtidEventParser.java
@@ -1,14 +1,28 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.code.or.binlog.impl.parser;
+
+import java.io.IOException;
 
 import com.google.code.or.binlog.BinlogEventV4Header;
 import com.google.code.or.binlog.BinlogParserContext;
 import com.google.code.or.binlog.impl.event.GtidEvent;
 import com.google.code.or.common.util.MySQLConstants;
 import com.google.code.or.io.XInputStream;
-
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
 
 /**
  * GTID Event
@@ -27,23 +41,27 @@ import java.nio.ByteOrder;
  *         +-------------------+
  *     </pre>
  * </p>
+ * @author brandtg
  */
 public class GtidEventParser extends AbstractBinlogEventParser {
 
-  public GtidEventParser() {
-    super(MySQLConstants.GTID_LOG_EVENT);
-  }
+	/**
+	 * 
+	 */
+	public GtidEventParser() {
+		super(MySQLConstants.GTID_LOG_EVENT);
+	}
 
-  @Override
-  public void parse(XInputStream is, BinlogEventV4Header header, BinlogParserContext context) throws IOException {
-    is.readBytes(1); // commit flag, always true
-    byte[] sourceId = is.readBytes(16);
-    long transactionId = ByteBuffer.wrap(is.readBytes(8)).order(ByteOrder.LITTLE_ENDIAN).getLong();
-    is.skip(is.available()); // position at next event
-
-    GtidEvent event = new GtidEvent(sourceId, transactionId);
-    event.setHeader(header);
-
-    context.getEventListener().onEvents(event);
-  }
+	/**
+	 * 
+	 */
+	public void parse(XInputStream is, BinlogEventV4Header header, BinlogParserContext context) throws IOException {
+		GtidEvent event = new GtidEvent(header);
+		is.readBytes(1); // commit flag, always true
+		event.setSourceId(is.readBytes(16));
+		event.setTransactionId(is.readLong(8, true));
+		//event.setTransactionId(ByteBuffer.wrap(is.readBytes(8)).order(ByteOrder.LITTLE_ENDIAN).getLong());
+		is.skip(is.available()); // position at next event
+		context.getEventListener().onEvents(event);
+	}
 }
